32207f6706bb2dbba318b7386c19989af898c884
Sieun Park <park12010@naver.com>
2023-09-02 11:29:30 +0900

    Initial commit

---
+++ baseball_ticket_sale/src/main/java/com/example/baseball_ticket_sale/BaseballTicketSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseball_ticket_sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballTicketSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballTicketSaleApplication.class, args);
+	}
+
+}
---
+++ baseball_ticket_sale/src/test/java/com/example/baseball_ticket_sale/BaseballTicketSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseball_ticket_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballTicketSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
