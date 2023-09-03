755702a12f56176a4a37e704b3ce8f52b3fb2f48
kimtaewoo0907 <kimtw0907@naver.com>
2023-09-03 09:58:34 +0900

    9/3

--- baseball_ticket_sale/src/main/java/com/example/baseball_ticket_sale/domain/Cancel.java
+++ baseball_ticket_sale/src/main/java/com/example/baseball_ticket_sale/domain/Cancel.java
@@ -1,4 +1,31 @@
 package com.example.baseball_ticket_sale.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Cancel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cancelNumber;
+
+    private String cancelStatus;
+
+    private int  refundFee;
+
+    private String refundStatus;
+
+    @OneToOne
+    @JoinColumn(name="match_day")
+    private Match match;
+
+    @OneToOne
+    @JoinColumn(name="reservation_number")
+    private Reservation reservation;
+
 }
--- baseball_ticket_sale/src/main/java/com/example/baseball_ticket_sale/domain/Reservation.java
+++ baseball_ticket_sale/src/main/java/com/example/baseball_ticket_sale/domain/Reservation.java
@@ -1,4 +1,36 @@
 package com.example.baseball_ticket_sale.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationNumber;
+
+    private String orderStatus;
+
+    private String refundStatus;
+
+    private String refundMoney;
+
+    LocalDateTime createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_number")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="payment_number")
+    private Payment payment;
+
+
+
 }
